7ae2e3aada9e768cddeb0ddcd0868c06bf13fc7e
sanchit <sanchit10bhasin@gmail.com>
2021-02-14 02:59:47 +0530

    created Palette component

---
+++ src/Palette.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class Palette extends Component {
+    render() {
+        return (
+            <div className="Palette">
+                <h1>Palette</h1>
+            </div>
+        );
+    }
+}
+
+export default Palette;
