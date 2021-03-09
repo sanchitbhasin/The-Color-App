b9f0a5ff878d184de63aea92725b05882ed13712
sanchit <sanchit10bhasin@gmail.com>
2021-03-09 19:37:29 +0530

    Tweak Styles of PaletteForm

--- src/styles/NewPaletteFormStyles.js
+++ src/styles/NewPaletteFormStyles.js
@@ -17,6 +17,7 @@ const styles = theme => ({
   },
   drawerHeader: {
     display: 'flex',
+    width: "100%",
     alignItems: 'center',
     padding: theme.spacing(0, 1),
     // necessary for content to be below app bar
@@ -26,7 +27,7 @@ const styles = theme => ({
   content: {
     flexGrow: 1,
     height: "calc(100vh - 64px)",
-    padding: theme.spacing(3),
+    padding: 0,
     transition: theme.transitions.create('margin', {
       easing: theme.transitions.easing.sharp,
       duration: theme.transitions.duration.leavingScreen,
